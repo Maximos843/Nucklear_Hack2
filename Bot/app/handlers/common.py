a4ae7b8c4137a56a1f3a2e8b76870a20b753b1a1
Носов Артём Иванович <nai008@campus.mephi.ru>
2024-04-21 13:54:29 +0300

    Pre done

--- Bot/app/handlers/common.py
+++ Bot/app/handlers/common.py
@@ -7,7 +7,7 @@ from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
 
 from Model_1.model_1 import *
-from model.model import *
+from model.model import MetroDataExtractor
 
 from app.db.functions import get_passenger_flow_from_db
 from app.db.functions import get_agg_passenger_flow_from_db
@@ -16,6 +16,7 @@ from app.db.functions import get_agg_passenger_flow_from_db
 class Diologue(StatesGroup):
     waiting_for_question = State()
     answer_for_question = State()
+    waiting_for_question_model_2 = State()
     agg_period = State()
     agg_period_2 = State()
     agg_period_3 = State()
@@ -122,17 +123,18 @@ stations = ["Яхромская", "Ясенево", "Южная", "Юго-Зап
             "Александровский сад", "Академическая",
             "Административная", "Автозаводская", "Авиамоторная"]
 
-get_station_fromat = '''Получи из текста название станций московского метро>. Если это невозможно то верни None. 
-Слово ответ в свой ответ не включай.
+get_station_fromat = '''Получи из X название станций московского метро. Если это невозможно то верни None. 
 Примеры:
-Текст: Сколько была пассажиров на станции Царицыно вчера. Ответ: Царицыно
-Текст: {}'''
+Текст: "Сколько была пассажиров на станции Царицыно вчера." Ответ: Царицыно
+X = "{}"'''
 
 
 async def get_station(question):
     full_prompt = get_station_fromat.format(question)
     answer = get_chat_completion(giga_token, full_prompt)
     station = answer.json()['choices'][0]['message']['content']
+    station = station.replace('Ответ: ', '')
+    station = station.replace('.', '')
     print(station, station not in stations)
     if station not in stations:
         raise Exception('station not in stations')
@@ -141,16 +143,16 @@ async def get_station(question):
 
 
 get_date_prompt = '''
-Получи из текста дату в формате год-месяц-день, если это невозможно, то верни None.
-Сегодняшняя дата: {}
+Выдай из строки "{}" дату в формате год-месяц-день.
+Метаинформация = Сегодняшняя дата: {}.
 Примеры:
 1. Текст: 24 марта 2024 года. Ответ: 2024-03-24
-Текст: {}
+2. Текст: собака. Ответ: None
 '''
 
 
 async def get_date(question, today):
-    full_prompt = get_date_prompt.format(today, question)
+    full_prompt = get_date_prompt.format(question, today)
     answer = get_chat_completion(giga_token, full_prompt)
     date = answer.json()['choices'][0]['message']['content']
     date = date.replace('Ответ: ', '')
@@ -159,11 +161,11 @@ async def get_date(question, today):
 
 
 get_date_gap_prompt = '''
-Из текста ниже тебе необходимо понять на сколько дней идёт сдвиг относительно текущего врмени в днях
+Из строки "{}" ниже тебе необходимо понять на сколько дней идёт сдвиг относительно текущего врмени в днях
 Если речь идёт о будущем, то число положительное
 Если речь идёт о прошлом, то число отрицательное
-Сегодняшняя дата: {}
-Сегодняшний день недели: {}
+Сегодняшняя дата: {}. Да, речь идёт о текущем месяце.
+Сегодняшний день недели: {}.
 Примеры
 1. Текст: две недели назад. Ответ: -14
 2. Текст: через месяц. Ответ: 30
@@ -171,15 +173,13 @@ get_date_gap_prompt = '''
 5. Текст: вчера. Ответ: -1
 6. Текст: 21.04.2024 вторник прошлой недели. Ответ: -12
 7. Сегодняшний день недели: вторник Текст: прошлый понедельник. Ответ: -8
-
-Текст: {}
 '''
 
 
 async def get_date_gap(question, today):
     today_date = datetime.datetime.fromisoformat(today)
     weekday = today_date.weekday()
-    full_prompt = get_date_gap_prompt.format(today, weekday, question)
+    full_prompt = get_date_gap_prompt.format(question, today, weekday)
     answer = get_chat_completion(giga_token, full_prompt)
     text_answer_day = answer.json()['choices'][0]['message']['content']
     text_answer_day = text_answer_day.replace('.', '')
@@ -212,9 +212,9 @@ async def get_date_res_date(question, today):
     return res_text_date, final_int_day
 
 get_agg_prompt = '''
-Из текста пойми какую из агрегирующих функий [SUM, AVG, MIN, MAX, COUNT] нужно применить. 
+Из X пойми какую из агрегирующих функий [SUM, AVG, MIN, MAX, COUNT] нужно применить. 
 Выдай просто один из наиболее подходящих вариантов списка [SUM, AVG, MIN, MAX, COUNT].
-Текст: {}
+X = "{}"
 '''
 async def get_from_list(text, list_arr):
     for val in list_arr:
@@ -258,8 +258,46 @@ async def answer_question(message: types.Message, state: FSMContext):
         await message.answer(answer_prompt.format(today, res_text_date, final_int_day, station, str(str_passenger_flow)))
     except Exception as e:
         print(e)
-        await message.answer('Проверьте корректность вашего запроса')
+        if str(e) == '"None" in text_day and int_day is None':
+            await message.answer('Проверьте корректность вашего запроса. Укажите дату точнее')
+        else:
+            await message.answer('Проверьте корректность вашего запроса')
     await state.set_state(Diologue.waiting_for_question.state)
+
+async def get_station_model_2(question):
+    m2 = MetroDataExtractor()
+    return m2.extract_station(question)
+
+async def get_date_model_2(question):
+    m2 = MetroDataExtractor()
+    return m2.extract_date(question)
+
+async def hello_answer_question_model_2(message: types.Message, state: FSMContext):
+    await message.answer('Был осуществлён переход на нашу модель')
+    await state.set_state(Diologue.waiting_for_question_model_2.state)
+
+async def answer_question_model_2(message: types.Message, state: FSMContext):
+    question = message.text
+    await message.answer('Запрос обрабатывается нашей моделью...')
+    today = '2024-04-03'
+    try:
+        station = await get_station_model_2(question)
+        # запрос SQL
+        res_text_date = await get_date_model_2(question)
+        print('model_2')
+        print(f'station: {station}')
+        print(f'res_text_date: {res_text_date}')
+
+        passenger_flow = await get_passenger_flow_from_db(station, None, res_text_date)  # res_dict
+        str_passenger_flow = await get_str_passenger_flow(passenger_flow)
+        date_today = datetime.datetime.fromisoformat(today)
+        final_int_day = datetime.datetime.fromisoformat(res_text_date) - date_today
+        final_int_day = final_int_day.days
+        await message.answer(answer_prompt.format(today, res_text_date, final_int_day, station, str(str_passenger_flow)))
+    except Exception as e:
+        print(e)
+        await message.answer('Проверьте корректность вашего запроса')
+    await state.set_state(Diologue.waiting_for_question_model_2.state)
 
 
 async def secret_command(message: types.Message):
@@ -340,6 +378,8 @@ import re
 def register_hendlers_common(dp: Dispatcher, admin_id: int):
     dp.register_message_handler(cmd_start, commands='start', state='*')
     dp.register_message_handler(answer_question, regexp=re.compile(r"^[^/].*"), state=Diologue.waiting_for_question)
+    dp.register_message_handler(hello_answer_question_model_2, commands='start_our_model', state='*')
+    dp.register_message_handler(answer_question_model_2, state=Diologue.waiting_for_question_model_2)
     # dp.register_message_handler(model_answer, state=Diologue.answer_for_question)
     # dp.register_message_handler(cmd_cancel, commands='cancel', state='*')
     # dp.register_message_handler(cmd_cancel, Text(equals='отмена', ignore_case=True), state='*')
